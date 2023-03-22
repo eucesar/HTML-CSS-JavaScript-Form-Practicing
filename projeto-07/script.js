50ff870eba7020b7956d4800a27b9c3334bbc127
Cesar Iglesias <cesaribneto.job@gmail.com>
2023-03-22 20:01:31 -0300

    alteração no js

--- projeto-07/script.js
+++ projeto-07/script.js
@@ -1,3 +1,4 @@
+//MENSAGEM DE ERRO
 function mensagem () {
 
     /*erro*/ 
@@ -6,4 +7,67 @@ function mensagem () {
 
 }
 
+// Seleção de elementos
+
+const todoFormulario = document.querySelector("#formulario");
+const nascimento = document.querySelector("#nome");
+const contato = document.querySelector("#telefone");
+const email = document.querySelector("#email");
+const cep = document.querySelector("#cep");
+const endereco = document.querySelector("#endereco");
+const numero = document.querySelector("#numero");
+const complemento = document.querySelector("#complemento");
+const bairro = document.querySelector("#bairro");
+const cidade = document.querySelector("#cidade");
+const uf = document.querySelector("#estado");
+
+// Verificar se estpa vazio - função
+todoFormulario.addEventListener("submit", (event) => {
+event.preventDefault();
+
+
+if (nascimento.value === "")
+    alert("Por favor, preencha corretamente")
+   return; //para n continuar a função
+
+if (contato.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+if (email.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+if (cep.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+if (endereco.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+if (numero.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+if (complemento.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+if (bairro.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+if (cidade.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+if (uf.value === "")
+    alert("Por favor, preencha corretamente")
+    return; //para n continuar a função
+
+// Se todos os campos estiverem corretamente preenchidos. envie o form
+    todoFormulario.submit();
+
+})
 /**/ 
